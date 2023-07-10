30f4af932ee7c693f75aaf6efc06277e5b4f5edb
faisal amri <136825752+faisal0310@users.noreply.github.com>
2023-07-10 23:50:40 +0700

    Delete galonsi directory

--- galonsi/admin/home.php
+++
@@ -1,2 +0,0 @@
-<h2>Selamat Datang Di Halaman Home</h2>
-<pre><?php print_r($_SESSION); ?></pre>
--- galonsi/logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-session_start();
-
-session_destroy();
-
-echo "<script>alert('Anda Telah Logout');</script>";
-echo "<script>location='index.php';</script>";
- ?>
